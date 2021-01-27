f4fe11ee64ea5a936f67688f49337cdc3f21310c
hitsamtiammar123 <hitsamtiammmar@gmail.com>
2021-01-27 09:52:37 +0700

    change forceScheme to https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\App;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +26,8 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         date_default_timezone_set('Asia/Jakarta');
+        if(App::environment() === 'production'){
+            \URL::forceScheme('https');
+        }
     }
 }
